d4ec5eba7cbcf01f84a1ceaddaaf2b940facc50a
rebeccaksulisufi <rebeccaksulisufi@gmail.com>
2018-02-03 16:50:03 +0000

    Update

--- Unit 1 Challenge/UNIT 1 DRAFT 2.py
+++ Unit 1 Challenge/UNIT 1 DRAFT 2.py
@@ -1,17 +1,14 @@
 import random
 # Imports the random function
 
-
 board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 # Defines the board for the game as an array with numbers 0-8
-
 
 def checkforwin(c, place1, place2, place3):
     if board[place1] == c and board[place2]==c and board[place3]==c:
         return ("Win")
 # Function checks whether this is a win
 # If the three places are equal to a winning combination, it prints "Win"
-
 
 def checkforwin2(c):
     if checkforwin(c,0,1,2)=="Win":
@@ -32,7 +29,6 @@ def checkforwin2(c):
         return ("Win")
 # Function lists all winning combinations to check for a win after every turn
 
-
 def showboard():
     print ("-------------")
     print ("|", board[0], "|",board[1], "|",board[2], "|")
@@ -43,7 +39,6 @@ def showboard():
     print ("-------------")
 # This function uses the array for the board from before
 # Creates a visual board for the user to see
-
 
 print ("Welcome to Tic Tac Toe!")
 print ("You are noughts!!")
@@ -59,13 +54,11 @@ def tictactoe():
     # While loop defines a variable which holds a users input of what tile they would like between 0-8
     # While loop then converts the input to an integer value
 
-
         if board[userSpot] != "x" and board[userSpot] != "o":
             board[userSpot] = "x"
     # If statement checks if the tile where the user wanted to place their shape already has a shape in it
     # If there is no shape on that tile, the program places the shape there
     # If there is a shape there the else statement outputs to the user that the spot is taken
-
 
             if checkforwin2("x") == "Win":
                 showboard()
@@ -74,7 +67,6 @@ def tictactoe():
     # If statement checks if there is a win using the earlier defined function which lists all the winning combinations
     # If the user has a winning combination, it outputs the user has won
 
-
             availableSpot = True
             while availableSpot:
                 random.seed # Gives a random generator
@@ -82,7 +74,6 @@ def tictactoe():
                 availableSpot = False
     # While loop produces a random number for the computer to use as their tile selection
     # ONLY produces a number between 0-8
-
 
                 if board[computerOpponent] != 'o' and board[computerOpponent] != 'o':
                     board[computerOpponent] = 'o'
@@ -101,5 +92,4 @@ def tictactoe():
             print ("This spot is taken!")
         showboard()
     # This else statement follows the if statement from before for if the user enters a tile number where there is already a shape.
-
 tictactoe()
